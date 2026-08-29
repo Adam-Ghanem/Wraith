aa330531a2a6dd94a3e805461fb73d9ac651ef95
Adam Ghanem <adam.ghanem.it@gmail.com>
2026-08-29 19:07:58 +0100

    feat(discovery): mirror ARP pool API off Linux

--- internal/discovery/arp_other.go
+++ internal/discovery/arp_other.go
@@ -12,8 +12,13 @@ import (
 var errPlatformARPUnsupported = errors.New("ARP discovery is not implemented on this platform")
 
 type LinuxARPResolver struct{}
+type LinuxARPResolverPool struct{}
 
 func NewLinuxARPResolver(_ net.Interface) (*LinuxARPResolver, error) {
+	return nil, errPlatformARPUnsupported
+}
+
+func NewLinuxARPResolverPool(_ net.Interface, _ int) (*LinuxARPResolverPool, error) {
 	return nil, errPlatformARPUnsupported
 }
 
@@ -22,5 +27,13 @@ func (r *LinuxARPResolver) Resolve(_ context.Context, _ netip.Addr) (net.Hardwar
 }
 
 func (r *LinuxARPResolver) Close() error {
+	return nil
+}
+
+func (p *LinuxARPResolverPool) Resolve(_ context.Context, _ netip.Addr) (net.HardwareAddr, error) {
+	return nil, errPlatformARPUnsupported
+}
+
+func (p *LinuxARPResolverPool) Close() error {
 	return nil
 }
